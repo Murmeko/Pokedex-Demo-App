862cd58007891da0050fd7a50306e4493dd4df35
Murmeko <yigiterdinc@gmail.com>
2021-06-20 21:06:02 +0300

    Initial Commit

---
+++ Pokedex Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex Demo App
+//
+//  Created by Yiğit Erdinç on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
